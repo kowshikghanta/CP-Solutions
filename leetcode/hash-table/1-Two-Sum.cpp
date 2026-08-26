8978a3e6183da0f82ceffb3238f254bb08e345c6
Ghanta Kowshik <ghantakowshik73@gmail.com>
2026-08-26 19:38:07 +0530

    Add Two-Sum (hash-table)

--- leetcode/hash-table/1-Two-Sum.cpp
+++ leetcode/hash-table/1-Two-Sum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for (std::size_t i = 0; i < nums.size(); i++) {
-            for (std::size_t j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {
-                        static_cast<int>(i), 
-                        static_cast<int>(j)
-                    };
-                }
-            }
-        }
+        std::unordered_map<int, int> hash_map;
 
+        for (std::size_t i = 0; i < nums.size(); i++) {
+            if (hash_map.contains(target - nums[i])) {
+                return {
+                    hash_map[target - nums[i]], static_cast<int> (i)
+                };
+            }
+            hash_map[nums[i]] = static_cast<int> (i);
+        }
+        
         return {};
     }
 };
