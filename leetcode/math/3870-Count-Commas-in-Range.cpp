52d457b4cacb164cfc35a8db687dc23402715c85
Ghanta Kowshik <ghantakowshik73@gmail.com>
2026-09-08 15:26:25 +0530

    Add Count-Commas-in-Range (math)

---
+++ leetcode/math/3870-Count-Commas-in-Range.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        long long ans = 0;
+        long long start = 1000;
+        int commas = 1;
+
+        while (start <= n) {
+            long long end = min((long long)n, start * 1000 - 1);
+            ans += (end - start + 1) * commas;
+            start *= 1000;
+            commas++;
+        }
+
+        return ans;
+    }
+};
