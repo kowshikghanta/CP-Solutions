cb032c5663a4b0f4fd313a5cde6110ab35da29ca
Ghanta Kowshik <ghantakowshik73@gmail.com>
2026-09-08 15:49:32 +0530

    Add Sales-Analysis-III (database)

--- leetcode/database/1084-Sales-Analysis-III.sql
+++ leetcode/database/1084-Sales-Analysis-III.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select distinct p.product_id, p.product_name from product p left join sales s on p.product_id = s.product_id where s.sale_date between '2019-01-01' and '2019-03-31' and p.product_id not in (select p1.product_id from product p1 left join sales s1 on p1.product_id = s1.product_id where s1.sale_date not between '2019-01-01' and '2019-03-31');
+select distinct p.product_id, p.product_name from product p join sales s on p.product_id = s.product_id group by p.product_id having min(s.sale_date) >= '2019-01-01' and max(s.sale_date) <= '2019-03-31';
