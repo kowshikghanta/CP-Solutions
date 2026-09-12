bad8ee9e147a743d31c5edba2253f0809cfdb59d
Ghanta Kowshik <ghantakowshik73@gmail.com>
2026-09-12 12:26:17 +0530

    Add Maximum-Number-of-Events-That-Can-Be-Attended-II (binary-search)

---
+++ leetcode/binary-search/1751-Maximum-Number-of-Events-That-Can-Be-Attended-II.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int n;
+    vector<vector<int>> dp;
+public:
+    int maxValue(vector<vector<int>>& events, int k) {
+        n = events.size();
+        dp.assign(n, vector<int>(k, -1));
+
+        std::sort(
+            events.begin(),
+            events.end(),
+            [] (const vector<int>& a, const vector<int>& b) {
+                return a[0] < b[0];
+            }
+        );
+
+        return recursion(events, 0, 0, k);
+    }
+    int recursion(vector<vector<int>>& events, int i, int total, int k) {
+        if (i >= n || total == k) {
+            return 0;
+        }
+        if (dp[i][total] != -1) {
+            return dp[i][total];
+        }
+        int skip = recursion(events, i + 1, total, k);
+
+        int next = std::lower_bound(
+            events.begin() + i + 1,
+            events.end(),
+            events[i][1],
+            [] (const vector<int>& event, int endTime) {
+                return event[0] <= endTime;
+            }
+        ) - events.begin();
+
+        int pick = events[i][2] + recursion(events, next, total + 1, k);
+
+        dp[i][total] = std::max(pick, skip);
+        return dp[i][total];
+    }
+};
